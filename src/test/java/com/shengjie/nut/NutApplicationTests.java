e5a313635c3dcc6f226b04819435771fa9955452
Alschairn <hong_email@icloud.com>
2020-04-04 14:44:43 +0800

    add

---
+++ src/test/java/com/shengjie/nut/NutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shengjie.nut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
