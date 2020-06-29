f8508f1b11ed69f97a8662d4eaf7313319645dea
Alschairn <hong_email@icloud.com>
2020-06-29 19:33:12 +0800

    fix: 优化脚本

--- jvm-go/jvm-go.sh
+++ jvm-go/jvm-go.sh
@@ -5,4 +5,4 @@ export GOPATH=$(cd `dirname $0`; pwd)
 export GOBIN=$(cd `dirname $0`; pwd)/bin
 
 # 构建main函数
-cd $(cd `dirname $0`; pwd)/src/main && go build main.go
+go build $(cd `dirname $0`; pwd)/src/main/main.go
