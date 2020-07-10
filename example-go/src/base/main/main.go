1120472ebbe00db186c72c64e29def2e57411d69
Alschairn <hong_email@icloud.com>
2020-07-10 15:49:57 +0800

    add: 添加基础语法

---
+++ example-go/src/base/main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	fmt.Printf("hello world! \n")
+
+	// for
+	for i := 0; i < 10; i++ {
+		fmt.Printf("for test %v \n", i)
+	}
+
+
+	// while
+	sum := 0
+	for sum < 10 {
+		fmt.Printf("while test %v \n", sum)
+		sum++
+	}
+
+	// 类型转换
+}
+
