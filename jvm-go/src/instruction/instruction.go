0412f00b45548023365afb6dcff709e0def44e57
Alschairn <hong_email@icloud.com>
2020-07-31 14:04:11 +0800

    add: 添加指令相关

--- jvm-go/src/instruction/instruction.go
+++ jvm-go/src/instruction/instruction.go
@@ -2,17 +2,42 @@ package instruction
 
 import "runtimedata"
 
+/**
+	指令接口
+ */
 type Instruction interface {
-
 	FetchOperands(reader *BytecodeReader)
 
 	Execute(frame runtimedata.Frame)
 }
 
 type NoOperandsInstruction struct {
-
 }
 
 func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
 	// nothing to do
+}
+
+type BranchInstruction struct {
+	Offset int // 偏移量
+}
+
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+	self.Offset = int(reader.ReadInt16())
+}
+
+type Index8Instruction struct {
+	Index uint // 局部变量表索引
+}
+
+func (self *Index8Instruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint8())
+}
+
+type Index16Instruction struct {
+	Index uint
+}
+
+func (self *Index16Instruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint16())
 }
