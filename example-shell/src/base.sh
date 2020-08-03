a975436fc37fa7fb30c12cf112659c5538f04736
Alschairn <hong_email@icloud.com>
2020-08-03 18:15:38 +0800

    add: 添加shell基础

--- example-shell/src/base.sh
+++ example-shell/src/base.sh
@@ -1,2 +1,27 @@
 #!/usr/bin/env bash
 
+# 获取当前执行文件绝对路径
+BASE_PATH=$(cd "$(dirname "$0")"; pwd)
+echo ${BASE_PATH}
+
+# 输出函数(两种方式)，多字符拼接
+echo `date '+%Y-%m-%d %H:%M:%S'` ' append1 ' ' append2  '
+echo $(date '+%Y-%m-%d %H:%M:%S') ' append1 ' ' append2  '
+
+# 覆盖写
+for VAR in 1 2 3 ; do
+    echo ${VAR} > ./temp.txt
+done
+cat ${BASE_PATH}/temp.txt
+
+# 清空文件
+cat /dev/null > ${BASE_PATH}/temp.txt
+
+# 追加写
+for VAR in 1 2 3 ; do
+    echo ${VAR} >> ${BASE_PATH}/temp.txt
+done
+
+cat ${BASE_PATH}/temp.txt
+
+rm -f ${BASE_PATH}/temp.txt
