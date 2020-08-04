4557dffb2bb5c6940babc9ef8a46c89cd4ba7583
Alschairn <hong_email@icloud.com>
2020-08-04 17:18:58 +0800

    update: 修改build文件

--- jvm-go/jvm-go.sh
+++ jvm-go/jvm-go.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+base_addr=$(cd `dirname $0`; pwd)
+echo '执行文件目录: ' ${base_addr}
 
-# 设置go path到当前目录
-export GOPATH=$(cd `dirname $0`; pwd)
-export GOBIN=$(cd `dirname $0`; pwd)/bin
+go_path=${base_addr}
+export GOPATH=${go_path}
+echo 'go path: ' ${go_path}
 
-# 构建main函数
-go build $(cd `dirname $0`; pwd)/src/main/main.go
+go_bin=${base_addr}/bin
+export GOBIN=${go_bin}
+echo 'go bin: ' ${go_bin}
 
+main_path=${base_addr}/src/main/main.go
+go build ${main_path}
+echo '主函数: ' ${main_path}
 
